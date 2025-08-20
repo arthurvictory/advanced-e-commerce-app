b009c84796404e59ddfe11c0d0a9f312abb402ce
Arthur Victory <arthurvictory@outlook.com>
2025-08-20 15:24:05 -0500

    update function to add total price and quantity to cart page

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -4,30 +4,73 @@ import {
   removeFromCart,
   updateQuantity,
   clearCart,
+  selectCartItems,
+  selectTotalQuantity,
+  selectTotalPrice
 } from "../features/cart/cartSlice";
 
 const CartItems = () => {
   const cart = useSelector((state: RootState) => state.cart.items);
+  
+  const shoppingCart = useSelector(selectCartItems);
+  const totalQuantity = useSelector(selectTotalQuantity);
+  const totalPrice = useSelector(selectTotalPrice);
+  
   const dispatch = useDispatch<AppDispatch>();
 
   return (
-    <div className="d-flex flex-column align-items-center shadow gap-3 col-md-3 p-3">
-      <h1>Cart:</h1>
-      {cart.map((item) => (
-        <div key={item.id}>
-          <img src={item.image} alt={item.title} className="w-25" />
-          <p>
-            {item.title} - Qty: {item.quantity}
-          </p>
-          <button onClick={() => dispatch(updateQuantity({ id: item.id, quantity: item.quantity - 1 }))}> - </button>
-          <button onClick={() => dispatch(removeFromCart(item.id))}>
-            Remove from cart
-          </button>
-          <button onClick={() => dispatch(updateQuantity({ id: item.id, quantity: item.quantity + 1 }))}> + </button>
-        </div>
-      ))}
-      <button onClick={() => dispatch(clearCart())}>Clear the cart</button>
-    </div>
+    <>
+      <div className="d-flex flex-row align-items-center">
+        <h1>Shopping Cart:</h1>
+        {cart.map((item) => (
+          <div key={item.id}>
+            <img src={item.image} alt={item.title} className="w-25" />
+            <p>{item.price}</p>
+            <p>
+              {item.title} - Qty: {item.quantity}
+            </p>
+            <button
+              onClick={() =>
+                dispatch(
+                  updateQuantity({ id: item.id, quantity: item.quantity - 1 })
+                )
+              }
+            >
+              {" "}
+              -{" "}
+            </button>
+            <button onClick={() => dispatch(removeFromCart(item.id))}>
+              Remove from cart
+            </button>
+            <button
+              onClick={() =>
+                dispatch(
+                  updateQuantity({ id: item.id, quantity: item.quantity + 1 })
+                )
+              }
+            >
+              {" "}
+              +{" "}
+            </button>
+          </div>
+        ))}
+        <button onClick={() => dispatch(clearCart())}>Clear the cart</button>
+      </div>
+      <div>
+        <h2>Total items:</h2>
+        <p>{totalQuantity}</p>
+        <h2>Total price:</h2>
+        <p>${totalPrice.toFixed(2)}</p>
+
+        {shoppingCart.map((item) => (
+          <div key={item.id}>
+            <p>
+              {item.id} — Qty: {item.quantity} — ${item.price}
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -2,6 +2,7 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { CartItem } from "../../types/type";
 import { saveToSession, loadFromSession } from "../../utilities/sessionStorage";
+import type { RootState } from "../../app/store";
 
 const initialCart = loadFromSession<CartItem[]>("cart") || [];
 
@@ -26,21 +27,35 @@ const cartSlice = createSlice({
       saveToSession("cart", state.items);
     },
     updateQuantity(
-      state,
-      action: PayloadAction<{ id: number; quantity: number }>
-    ) {
-      const item = state.items.find((i) => i.id === action.payload.id);
-      if (item) {
-        item.quantity = action.payload.quantity;
-        saveToSession("cart", state.items);
-      }
-    },
+      state, action: PayloadAction<{ id: number; quantity: number }>) {
+        const { id, quantity } = action.payload;
+        if (quantity <= 0) {
+          //Remove the item from the cart
+          state.items = state.items.filter((i) => i.id !== id);
+        } else {
+          const item = state.items.find((i) => i.id === id);
+          if(item) {
+            item.quantity = quantity;
+          }
+        }
+
+        saveToSession('cart', state.items)
+      },
+
     clearCart(state) {
       state.items = [];
       saveToSession("cart", []);
     },
   },
 });
+
+export const selectCartItems = (state: RootState) => state.cart.items;
+
+export const selectTotalQuantity = (state: RootState): number =>
+  state.cart.items.reduce((total, item ) => total + item.quantity, 0);
+
+export const selectTotalPrice = (state: RootState): number =>
+  state.cart.items.reduce((total, item) => total + item.price * item.quantity, 0);
 
 export const { addToCart, removeFromCart, updateQuantity, clearCart } =
   cartSlice.actions;
